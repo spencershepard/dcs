28ef0a448691311b48299615b67679744d4dd70d
Dan Albert <dan@gingerhq.net>
2023-05-12 18:50:26 -0700

    Test new country APIs.

--- tests/test_country.py
+++ tests/test_country.py
@@ -5,3 +5,11 @@ def test_countries_eq() -> None:
     assert dcs.countries.get_by_id(0) == dcs.countries.get_by_id(0)
     assert dcs.countries.get_by_id(0) != dcs.countries.get_by_id(1)
     assert dcs.countries.get_by_id(0) != "dynamic typing is stupid"
+
+
+def test_country_by_name() -> None:
+    dcs.countries.get_by_name("Russia") == dcs.countries.get_by_id(0)
+
+
+def test_country_by_short_name() -> None:
+    dcs.countries.get_by_short_name("RUS") == dcs.countries.get_by_id(0)
